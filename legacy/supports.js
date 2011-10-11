2b60f4f83f984987e8cf64a88af8a9adfe06cfef
Simon <sstur@me.com>
2011-10-12 08:51:07 +1000

    refactored

--- legacy/supports.js
+++ legacy/supports.js
@@ -1,27 +1,22 @@
-(function(win, doc) {
+(function(win) {
 
   function supports(feature) {
     var supported = false, m;
     if (feature == 'placeholder') {
-      var el = doc.createElement('input');
+      var el = document.createElement('input');
       supported = ('placeholder' in el);
     } else
     if (feature && (m = feature.match(/flash\s*(v\d+)?(\.\d+)?/i))) {
       var major = m[1] && m[1].replace('v', ''), minor = m[2] && m[2].replace('.', '');
-      supported = hasFlashVersion(major || 1, minor);
+      supported = flashVersion(major || 1, minor);
     }
     return supported;
   }
 
   //Helper Functions
-  function hasFlashVersion(major, minor) {
+  function flashVersion(major, minor) {
     minor = minor || 0;
-    var ver = getFlashVersion();
-    return (ver) ? ver[0] > major || ver[0] == major && ver[1] >= minor : false;
-  }
-
-  function getFlashVersion() {
-    var ver, nothing;
+    var ver;
     if (navigator.plugins && navigator.plugins.length > 0) {
       var types = navigator.mimeTypes, type = 'application/x-shockwave-flash';
       if (types && types[type] && types[type].enabledPlugin && types[type].enabledPlugin.description) {
@@ -30,15 +25,18 @@
     } else {
       try {
         var fv = new ActiveXObject('ShockwaveFlash.ShockwaveFlash').getVariable('$version');
-      } catch(e) { return nothing; }
+      } catch(e) { return false; }
       ver = fv.replace(/.*?([0-9]+,[0-9]+).*/, '$1').split(',');
     }
-    return (ver) ? [parseInt(ver[0], 10), parseInt(ver[1], 10)] : nothing;
+    if (ver) {
+      ver[0] = parseInt(ver[0], 10);
+      return ver[0] > major || ver[0] == major && parseInt(ver[1], 10) >= minor;
+    }
+    return false;
   }
 
   //Exports
-  supports.hasFlashVersion = hasFlashVersion;
-  supports.getFlashVersion = getFlashVersion;
+  supports.flashVersion = flashVersion;
   win.supports = supports;
 
-})(window, document);
+})(window);
